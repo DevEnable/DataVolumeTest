4d37c93d30f9ac435e63a6374ca52d254bb255fe
Stephen Kennedy <stephen@devenable.com>
2016-06-03 11:34:13 +1000

    Added shell DatabaseSetup

--- DatabaseSetup/Program.cs
+++ DatabaseSetup/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
+using DbUp;
+using DbUp.Engine;
 
 namespace DatabaseSetup
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+
+            UpgradeEngine engine =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            if (!engine.IsUpgradeRequired())
+            {
+                Console.WriteLine("No upgrade required, database is already up to date.");
+                return 0;
+            }
+
+            return PerformUpgrade(engine);
+        }
+
+        private static int PerformUpgrade(UpgradeEngine engine)
+        {
+            var result = engine.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
         }
     }
 }
