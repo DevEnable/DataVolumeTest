93c2429e776674c163dd8ae272cfaa25c87cd65f
Stephen Kennedy <stephen@devenable.com>
2016-06-16 11:08:42 +1000

    Added sp_InsertTVP

---
+++ DatabaseSetup/Scripts/Script-00004 - InsertTVP.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE sp_InsertTVP
+	@tvp LightTestTVP READONLY,
+	@lookup LightLookup READONLY,
+	@date DateTime
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[TestTable](ColumnA, ColumnB, ColumnC, ColumnD, ColumnE, ColumnF, ColumnG, ColumnH, ColumnI)
+		SELECT t.ColumnA, l.RepeatableB, l.RepeatableC, l.RepeatableD, t.ColumnE, l.RepeatableF, l.RepeatableG, @date, @date
+		FROM @tvp AS t
+		INNER JOIN @lookup l ON t.RepeatableId = l.ID
+END
